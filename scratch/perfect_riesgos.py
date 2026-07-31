b1b95892251ac09600560f95a6247a9ac0666362
orbesrc-lab <orbesrc@gmail.com>
2026-07-30 23:50:02 -0500

    fix: restore 5x5 Heatmap matrix in Riesgos and add visible interactive edit/delete action buttons

--- scratch/perfect_riesgos.py
+++ scratch/perfect_riesgos.py
@@ -1,4 +1,6 @@
-<!DOCTYPE html>
+import os
+
+file_content = """<!DOCTYPE html>
 <html lang="es">
 <head>
     <meta charset="UTF-8">
@@ -420,9 +422,9 @@
 
                 <script>
                     window.onload = function() { setTimeout(() => { window.print(); }, 500); };
-                <\/script>
-            <\/body>
-            <\/html>
+                <\\/script>
+            <\\/body>
+            <\\/html>
             `;
 
             const win = window.open('', '_blank');
@@ -431,3 +433,9 @@
     </script>
 </body>
 </html>
+"""
+
+with open(r'c:\SIAC\templates\empresa_riesgos.html', 'w', encoding='utf-8') as f:
+    f.write(file_content)
+
+print("empresa_riesgos.html perfected with heatmap and edit/delete icons!")
